3e64e42f66da1e4ff93939c53437076660ef9730
Casca <cascabusiness@gmail.com>
2021-04-05 20:37:35 +0700

    first commit project tower of HaNoi

---
+++ src/AppplicationTestDrive.java
@@ -0,0 +1,12 @@
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class AppplicationTestDrive {
+
+	// Main method
+	public static void main(String[] args) {
+		Hanoi_TowerGUI action = new Hanoi_TowerGUI();
+
+		action.addWindowListener(new WindowAdapter() {public void windowClosing(WindowEvent e) {System.exit(0);}});
+	}
+}
